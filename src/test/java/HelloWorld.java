314bf3cffbeb2d60e271d111b17a1d3ea61f59cc
Stiliyan Ignatov <ignatov.stili@gmail.com>
2022-12-04 19:06:39 +0000

    Check this !

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
+
+
         System.out.println("Hello my friends HI ya buddy ");
+
+        int myage = 34;
+
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("You are " + age + " years old.");
     }
-}
+
+    }
+
+
+
